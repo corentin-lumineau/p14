870af30695434f90e8eb29975420e069c029d970
corentin <63396946+corentin-lumineau@users.noreply.github.com>
2023-05-29 16:01:53 +0200

    final commit

--- src/components/Row/index.jsx
+++
@@ -1,9 +0,0 @@
-import '../../style/components/Table.css'
-
-export default function Row({data}) {
-    return(
-        data.map((el) => (
-            <p className="row-element">{el}</p>
-        ))
-    )
-}
--- src/pages/EmployeeList/index.jsx
+++ src/pages/EmployeeList/index.jsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux"
 import { selectEmployeeList } from "../../utils/selector"
-import Table from '../../components/Table/index.jsx'
+import { Table } from "cl-table-library"
 
 export default function EmployeeList() {
 
     const employeeList = useSelector(selectEmployeeList)
+    debugger;
   
     return (
-        <Table  data={employeeList}/>
+        <Table data={employeeList} />
     )
 }
